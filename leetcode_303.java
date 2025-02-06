3235e799a3507bb92bdb861c69979fc27affa13a
kanishkhaacse <kanishkhaa.cs23@bitsathy.ac.in>
2025-02-06 06:41:29 +0530

    solved

---
+++ leetcode_303.java
@@ -0,0 +1,19 @@
+class NumArray {
+    int[] prefix;
+   public NumArray(int[] nums) {
+        for(int i=1;i<nums.length;i++)
+        {
+            nums[i] = nums[i-1]+ nums[i];
+        }
+        this.prefix=nums;
+    }
+    
+    public int sumRange(int left, int right) 
+    {
+        if(left==0)
+        {
+            return prefix[right];
+        }
+        return prefix[right]-prefix[left-1];
+    }
+}
